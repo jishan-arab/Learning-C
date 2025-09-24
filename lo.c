abd0ef1f04447fce8d49b3fc4c70a2e6dc06491c
jishan_arab <jishan0arab@gmail.com>
2025-09-24 18:02:20 +0530

    scanf

--- lo.c
+++ lo.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 void main () {
+int age;
+double h;
+printf("your age and hegitttt");
 
-printf("elvish bhaiii");
+scanf("%d %lf" , &age ,&h);
+
+printf("%d \n %lf" , age , h );
 
 }
