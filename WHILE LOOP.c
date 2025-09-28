a3cda8a9306afbc1e50e9b3f62bfdb8cc70b2b12
WATER <jishanarab09@gmail.com>
2025-09-28 15:45:07 +0530

    Create WHILE LOOP.c
    use int =1;
    
    //code
    
    i++;
    
    for counting/counter for loop 
    (how much time i want to run loop)

---
+++ WHILE LOOP.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main () {
+int a,b=0;
+printf("give me a number");
+scanf("%d" , &a);
+
+int i = 1;
+while(i<=a)
+{
+       
+     b=b+2;
+        printf("%d\n" , b);
+      i++;   
+}
+
+
+}
